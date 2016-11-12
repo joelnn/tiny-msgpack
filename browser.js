ca99dfe17fe32546d8f30db4a72f2c19d965d110
Joshua Wise <joshuathomaswise@gmail.com>
2016-11-12 17:06:53 -0500

    renamed buffer to bufferutil

--- browser.js
+++ browser.js
@@ -2,6 +2,6 @@
 module.exports = require('./lib/msgpack');
 
 var decoder = new TextDecoder('utf-8');
-require('./lib/buffer').toString = function (buffer, start, end) {
+require('./lib/buffer-util').toString = function (buffer, start, end) {
 	return decoder.decode(buffer.subarray(start, end));
 };
--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 module.exports = require('./lib/msgpack');
 
 var toString = Buffer.prototype.toString;
-require('./lib/buffer').toString = function (buffer, start, end) {
+require('./lib/buffer-util').toString = function (buffer, start, end) {
 	return toString.call(buffer, 'utf8' start, end);
 };
