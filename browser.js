3ace5fc0ca66c15a5f278729ca06b267e400bc4f
Joshua Wise <joshuathomaswise@gmail.com>
2016-11-13 16:08:14 -0500

    string encoding

--- browser.js
+++ browser.js
@@ -1,8 +1,12 @@
 'use strict';
 
-var decoder = new TextDecoder('utf-8');
+var decoder = new TextDecoder;
+var encoder = new TextEncoder;
 require('./lib/buffer-util').toString = function (buffer, start, end) {
 	return decoder.decode(buffer.subarray(start, end));
+};
+require('./lib/buffer-util').fromString = function (string) {
+	return encoder.encode(string);
 };
 
 module.exports = require('./lib/msgpack');
