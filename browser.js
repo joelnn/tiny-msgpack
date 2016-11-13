26b28547a3ed86a61bc3d02f82e20ef7e61a9189
Joshua Wise <joshuathomaswise@gmail.com>
2016-11-12 20:01:04 -0500

    tweaks

--- browser.js
+++ browser.js
@@ -1,7 +1,8 @@
 'use strict';
-module.exports = require('./lib/msgpack');
 
 var decoder = new TextDecoder('utf-8');
 require('./lib/buffer-util').toString = function (buffer, start, end) {
 	return decoder.decode(buffer.subarray(start, end));
 };
+
+module.exports = require('./lib/msgpack');
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
-module.exports = require('./lib/msgpack');
 
 var toString = Buffer.prototype.toString;
 require('./lib/buffer-util').toString = function (buffer, start, end) {
 	return toString.call(buffer, 'utf8' start, end);
 };
+
+module.exports = require('./lib/msgpack');
