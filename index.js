1a7ee9323549b3c4ffdd591472d136d7146b6e56
Joshua Wise <joshuathomaswise@gmail.com>
2016-11-12 21:19:16 -0500

    tweaks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var toString = Buffer.prototype.toString;
 require('./lib/buffer-util').toString = function (buffer, start, end) {
-	return toString.call(buffer, 'utf8' start, end);
+	return toString.call(buffer, 'utf8', start, end);
 };
 
 module.exports = require('./lib/msgpack');
